5f5528af21a317269c6ab8003834dc2b0d4da05f
manorit2001 <manorit2001@gmail.com>
2020-01-07 09:45:22 +0530

    autosuggest working fine

--- app/src/main/java/com/example/mapstest/tempmaps.java
+++ app/src/main/java/com/example/mapstest/tempmaps.java
@@ -12,6 +12,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -20,6 +21,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -41,13 +43,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.AutocompletePrediction;
 import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
+import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceResponse;
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.mancj.materialsearchbar.MaterialSearchBar;
+import com.mancj.materialsearchbar.adapter.SuggestionsAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class tempmaps extends FragmentActivity implements OnMapReadyCallback {
@@ -76,7 +83,8 @@ public class tempmaps extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
         mapView = mapFragment.getView();
         mFusedLocationProviderCLient = LocationServices.getFusedLocationProviderClient(tempmaps.this);
-***REMOVED***
+        if(!Places.isInitialized())
+            Places.initialize(getApplicationContext(), getString(R.string.google_maps_key));
         placesclient = Places.createClient(this);
         final AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
 
@@ -88,7 +96,11 @@ public class tempmaps extends FragmentActivity implements OnMapReadyCallback {
 
             @Override
             public void onSearchConfirmed(CharSequence text) {
-                startSearch(text.toString(), true, null, true);
+                startSearch(text.toString(), true, null, false);
+//                if(!materialSearchBar.isSuggestionsVisible())
+//                    materialSearchBar.showSuggestionsList();
+//                else
+//                    materialSearchBar.hideSuggestionsList();
             }
 
             @Override
@@ -102,6 +114,7 @@ public class tempmaps extends FragmentActivity implements OnMapReadyCallback {
             }
         });
 
+
         materialSearchBar.addTextChangeListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -110,34 +123,101 @@ public class tempmaps extends FragmentActivity implements OnMapReadyCallback {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                FindAutocompletePredictionsRequest predictionsRequest = FindAutocompletePredictionsRequest.builder().setCountry("in")
-                        .setTypeFilter(TypeFilter.ADDRESS).setSessionToken(token).setQuery(s.toString()).build();
+                FindAutocompletePredictionsRequest predictionsRequest = FindAutocompletePredictionsRequest.builder()
+                        .setQuery(s.toString()).setSessionToken(token).setCountry("in").setTypeFilter(TypeFilter.ADDRESS).build();
 //                if(predictionsRequest!=null)
-                placesclient.findAutocompletePredictions(predictionsRequest)
+                /*placesclient.findAutocompletePredictions(predictionsRequest)
                         .addOnCompleteListener(new OnCompleteListener<FindAutocompletePredictionsResponse>() {
-                                                                                                       @Override
-                                                                                                       public void onComplete(@NonNull Task<FindAutocompletePredictionsResponse> task) {
-                               if (task.isSuccessful()) {
-                                   FindAutocompletePredictionsResponse predictionsResponse = task.getResult();
-                                   if (predictionsResponse != null) {
-                                       predictionList = predictionsResponse.getAutocompletePredictions();
-                                       List<String> suggestionList = new ArrayList<>();
-                                       for (int i = 0; i < predictionList.size(); i++) {
-                                           AutocompletePrediction prediction = predictionList.get(i);
-                                           suggestionList.add(prediction.getFullText(null).toString());
-                                           Log.i("list", prediction.getFullText(null).toString());
+                               @Override
+                               public void onComplete(@NonNull Task<FindAutocompletePredictionsResponse> task) {
+                                   if (task.isSuccessful()) {
+                                       FindAutocompletePredictionsResponse predictionsResponse = task.getResult();
+                                       if (predictionsResponse != null) {
+                                           predictionList = predictionsResponse.getAutocompletePredictions();
+                                           List<String> suggestionList = new ArrayList<>();
+                                           for (AutocompletePrediction prediction : predictionsResponse.getAutocompletePredictions()) {
+//                                           for (int i = 0; i < predictionList.size(); i++) {
+//                                               AutocompletePrediction prediction = predictionList.get(i);
+                                               suggestionList.add(prediction.getFullText(null).toString());
+                                               Log.i("list", prediction.getFullText(null).toString());
+                                           }
+                                           materialSearchBar.updateLastSuggestions(suggestionList);
+                                           if (!materialSearchBar.isSuggestionsVisible()) {
+                                               materialSearchBar.showSuggestionsList();
+                                           }
                                        }
-                                       materialSearchBar.updateLastSuggestions(suggestionList);
-                                       if (!materialSearchBar.isSuggestionsVisible()) {
-                                           materialSearchBar.showSuggestionsList();
-                                       }
+                                   } else {
+                                       Log.i("mytag", "unable to perform predictions");
                                    }
-                               } else {
-                                   Log.i("mytag", "unable to perform predictions");
-                               }
-                           }
+                            }
                        }
                 );
+*/
+                String TAG;
+                TAG = new String("output");
+                placesclient.findAutocompletePredictions(predictionsRequest).addOnSuccessListener((response) -> {
+                    List<String> suggestionList = new ArrayList<>();
+                    predictionList = response.getAutocompletePredictions();
+                    for (AutocompletePrediction prediction : predictionList) {
+                        suggestionList.add(prediction.getFullText(null).toString());
+                        Log.i(TAG, prediction.getPlaceId());
+                        Log.i(TAG, prediction.getPrimaryText(null).toString());
+                    }
+                    materialSearchBar.updateLastSuggestions(suggestionList);
+                    if(!materialSearchBar.isSuggestionsVisible()){
+                        materialSearchBar.showSuggestionsList();
+                    }
+                }).addOnFailureListener((exception) -> {
+                    if (exception instanceof ApiException) {
+                        ApiException apiException = (ApiException) exception;
+                        Log.e(TAG, "Place not found: " + apiException.getStatusCode());
+                        Toast.makeText(tempmaps.this,"Place not found: " + apiException.getStatusCode(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                materialSearchBar.setSuggestionsClickListener(new SuggestionsAdapter.OnItemViewClickListener() {
+                    @Override
+                    public void OnItemClickListener(int position, View v) {
+                        if(position>predictionList.size())
+                            return;
+                        AutocompletePrediction selectedPrediction = predictionList.get(position);
+                        String suggestion= materialSearchBar.getLastSuggestions().get(position).toString();
+                        materialSearchBar.setText(suggestion);
+                        materialSearchBar.clearSuggestions();
+                        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                        if(imm!=null)
+                            imm.hideSoftInputFromWindow(materialSearchBar.getWindowToken(),InputMethodManager.HIDE_IMPLICIT_ONLY);
+                        String placeId= selectedPrediction.getPlaceId();
+                        List<Place.Field> placeFields = Arrays.asList(Place.Field.LAT_LNG);
+                        FetchPlaceRequest fetchPlaceRequest = FetchPlaceRequest.builder(placeId,placeFields).setSessionToken(token).build();
+                        placesclient.fetchPlace(fetchPlaceRequest).addOnSuccessListener(new OnSuccessListener<FetchPlaceResponse>() {
+                            @Override
+                            public void onSuccess(FetchPlaceResponse fetchPlaceResponse) {
+                                LatLng latLng = fetchPlaceResponse.getPlace().getLatLng();
+                                if(latLng != null)
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,DEFAULT_ZOOM));
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                if ( e instanceof ApiException)
+                                {
+                                    ApiException apiException = (ApiException) e;
+                                    apiException.printStackTrace();
+                                    Log.e("fetchplace","Place not found" + apiException.getStatusCode());
+                                    Toast.makeText(tempmaps.this,"Place not found" + apiException.getStatusCode(),Toast.LENGTH_SHORT);
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void OnItemDeleteListener(int position, View v) {
+
+
+                    }
+                });
+
             }
 
             @Override
@@ -233,8 +313,6 @@ public class tempmaps extends FragmentActivity implements OnMapReadyCallback {
                 }
             }
         });
-
-
     }
 
     @Override
