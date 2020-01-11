09356bdfaccb3d91d67785acceaf340453a83b09
Deepanshu Hooda <hoodaisdeepanshu@gmail.com>
2020-01-11 15:24:56 +0530

    Updated color.xml and cardview.xml

--- app/src/main/java/com/example/mapstest/MyWarehouseData.java
+++ app/src/main/java/com/example/mapstest/MyWarehouseData.java
@@ -4,10 +4,11 @@ public class MyWarehouseData {
 
 
 
-        static String[] nameArray = {"Cupcake", "Donut", "Eclair", "Froyo","azxczxc","b","c","d","e","f","g","h","i"};
+        static String[] nameArray = {"", "Donut", "Eclair", "Froyo","azxczxc","b","c","d","e","f","g","h","i"};
         static String[] distArray = {"1.5 km away", "1.6 km away", "2.0 km away", "2.2 km away","1.5 km away", "1.6 km away", "2.0 km away", "2.2 km away","1.5 km away", "1.6 km away", "2.0 km away", "2.2 km away"} ;
 
         static Integer[] drawableArray = {R.drawable.person2, R.drawable.person3, R.drawable.person1,R.drawable.paintbrush,R.drawable.img5 ,R.drawable.img6 ,R.drawable.img7,R.drawable.img8,R.drawable.img9,R.drawable.img10,R.drawable.img11 };
-        static String[] loc = {"here","fadsf","fasf","fasdf","here","fadsf","fasf","fasdf","here","fadsf","fasf","fasdf","here","fadsf","fasf","fasdf",};
-        static Integer[] id_ = {0, 1, 2, 3,4,5,6,7,8,9,10,11,12};
+//loc is now mobile number
+        static String[] loc = {"9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888",};
+        static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 }
