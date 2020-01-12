512c2e756cc1814c0b8a490f0a8cd783d8a409a1
Deepanshu Hooda <hoodaisdeepanshu@gmail.com>
2020-01-12 16:52:53 +0530

    Updated names yet again, although did it before, thanks to ashutosh , made activity_list_warehouse sexier

--- app/src/main/java/com/example/mapstest/MyWarehouseData.java
+++ app/src/main/java/com/example/mapstest/MyWarehouseData.java
@@ -4,11 +4,13 @@ public class MyWarehouseData {
 
 
 
-        public static String[] nameArray = {"Donut", "Eclair", "Froyo","azxczxc","b","c","d","e","f","g","h","i"};
-        public static String[] distArray = {"1.5 km", "1.6 km", "2.0 km", "2.2 km","1.5 km", "1.6 km", "2.0 km", "2.2 km","1.5 km", "1.6 km", "2.0 km", "2.2 km"} ;
+        public static String[] nameArray = {"Deepanshu", "Manorit", "Parth", "Ashutosh","Kanu Priya","Tanya","Prithvi","Karan","AShish","Aditya","Chaitanya","Vishal","Aakash"};
+        public static String[] distArray = {"1.5 km away", "1.6 km away", "2.0 km away", "2.2 km away","1.5 km away", "1.6 km away", "2.0 km away", "2.2 km away","1.5 km away", "1.6 km away", "2.0 km away", "2.2 km away"} ;
 
         public static int[] drawableArray = {R.drawable.person2, R.drawable.person3, R.drawable.person1,R.drawable.paintbrush,R.drawable.img5 ,R.drawable.img6 ,R.drawable.img7,R.drawable.img8,R.drawable.img9,R.drawable.img10,R.drawable.img11 };
-//loc is now mobile number
+        //loc is now mobile number
         public static String[] loc = {"9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888","9811281888",};
-        public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        public static int[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        //adding crops
+        public static String[] crops = {"Wheat , Barley , Paddy","Apples , Banana , Oranges","Lentils , Paddy, Carrots","Wheat , Barley , Paddy","Apples , Banana , Oranges","Lentils , Paddy, Carrots","Wheat , Barley , Paddy","Apples , Banana , Oranges","Lentils , Paddy, Carrots","Wheat , Barley , Paddy","Apples , Banana , Oranges","Lentils , Paddy, Carrots"} ;
 }
