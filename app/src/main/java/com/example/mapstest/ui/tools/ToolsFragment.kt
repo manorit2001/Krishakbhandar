429c54c78c1790cc147568a079d4b33c873661a4
Ashutosh Varma <ashutoshvarma11@live.com>
2020-01-13 10:56:26 +0530

    fix CAMERA permission for QR Code Scanner
    Signed-off-by: Ashutosh Varma <ashutoshvarma11@live.com>

--- app/src/main/java/com/example/mapstest/ui/tools/ToolsFragment.kt
+++ app/src/main/java/com/example/mapstest/ui/tools/ToolsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mapstest.ui.tools
 
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,12 @@ import com.example.mapstest.R
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.fragment_tools.*
+import com.tbruyelle.rxpermissions2.RxPermissions
+import android.Manifest.permission
+
+
+
+
 
 
 class ToolsFragment : Fragment() {
@@ -18,24 +25,35 @@ class ToolsFragment : Fragment() {
     private var disposable: Disposable? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_tools, container, false)
+        return inflater.inflate(com.example.mapstest.R.layout.fragment_tools, container, false)
     }
 
     override fun onStart() {
         super.onStart()
 
-        disposable = barcodeView
-                .drawOverlay()
-                .getObservable()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        {
-                            Toast.makeText(requireContext(), it.displayValue + " - Suceesfully Scanned QR Code", Toast.LENGTH_SHORT).show()
-                            activity?.onBackPressed()
-                        },
-                        {
-                            Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+        val rxPermissions = RxPermissions(this)
+        rxPermissions
+                .request(Manifest.permission.CAMERA)
+                .subscribe { granted ->
+                    if (granted!!) { // Always true pre-M
+                        // I can control the camera now
+                        disposable = barcodeView
+                                .drawOverlay()
+                                .getObservable()
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(
+                                        {
+                                            Toast.makeText(requireContext(), it.displayValue + " - Suceesfully Scanned QR Code", Toast.LENGTH_SHORT).show()
+                                            activity?.onBackPressed()
+                                        },
+                                        {
+                                            Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
                         })
+                    } else {
+                        activity?.onBackPressed();
+                    }
+                }
+
     }
 
     override fun onStop() {
